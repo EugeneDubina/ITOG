d71e57b4db93b81cae1f46b39f04d41611cbe43e
EugeneDubina <dubina.evgenij@gmail.com>
2022-12-28 11:22:03 +0300

    addTempAndTexta

--- Program.cs
+++ Program.cs
@@ -8,3 +8,19 @@ char s = ' ';
 string[] Words = text.Split(separators, StringSplitOptions.RemoveEmptyEntries);
 string[] tempWords = new string[Words.Length];
 string texta = tempWords[0];
+string[] TempArray(string[] inArray)
+{
+    for (i = 0; i < inArray.Length; i++)
+    {
+        inArray[i] = inArray[i] + s;
+        if (((inArray[i].LastIndexOf(s) - 1) <= 2))
+            tempWords[i] = inArray[i];
+    }
+    return (tempWords);
+}
+String Texta(string[] inArray)
+{
+    for (i = 0; i < inArray.Length; i++)
+        texta = texta + inArray[i];
+    return (texta);
+}
