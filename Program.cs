3c6fc58dccc13d30a45ed5e5d576ed030d2f0733
EugeneDubina <dubina.evgenij@gmail.com>
2022-12-28 11:21:15 +0300

    Body

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите перечень элементов массива");
+string text = Console.ReadLine();
+
+string[] separators = {" ", ",", ".", ";"};
+int i = 0;
+char s = ' ';
+
+string[] Words = text.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+string[] tempWords = new string[Words.Length];
+string texta = tempWords[0];
